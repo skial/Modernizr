e879111f6d88e05daa6fe661bc38472fb7fe388b
Skial Bainn <skial.bainn@gmail.com>
2010-09-11 15:14:20 +0100

    haXe javascript wrap of Modernizr. All methods and values done, apart from one. Check README for info.

---
+++ Main.hx
@@ -0,0 +1,60 @@
+﻿package ;
+
+import js.Lib;
+import com.modernizr.Modernizr;
+
+/**
+ * ...
+ * @author Skial Bainn
+ */
+
+class Main {
+	
+	static function main() {
+		trace('@font-face: 					' + Modernizr.fontface);
+		trace('canvas: 						' + Modernizr.canvas);
+		trace('canvas text: 					' + Modernizr.canvastext);
+		trace('html5 audio: 					' + Modernizr.audio);
+		trace('html5 audio format ogg: 	' + Modernizr.audio.ogg);
+		trace('html5 audio format wav: 	' + Modernizr.audio.wav);
+		trace('html5 audio format mp3: 	' + Modernizr.audio.mp3);
+		trace('html5 audio format m4a: 	' + Modernizr.audio.m4a);
+		trace('html5 video: 					' + Modernizr.video);
+		trace('html5 video format ogg: 	' + Modernizr.video.ogg);
+		trace('html5 video format h264: 	' + Modernizr.video.h264);
+		trace('rgba: 							' + Modernizr.rgba);
+		trace('hsla: 							' + Modernizr.hsla);
+		trace('border-image: 				' + Modernizr.borderimage);
+		trace('border-radius: 				' + Modernizr.borderradius);
+		trace('box-shadow: 					' + Modernizr.boxshadow);
+		trace('multiple-backgrounds: 		' + Modernizr.multiplebgs);
+		trace('backgroundsize: 				' + Modernizr.backgroundsize);
+		trace('opacity: 						' + Modernizr.opacity);
+		trace('css animations: 				' + Modernizr.cssanimations);
+		trace('css columns: 					' + Modernizr.csscolumns);
+		trace('css gradients: 				' + Modernizr.cssgradients);
+		trace('css reflections: 			' + Modernizr.cssreflections);
+		trace('css 2d transforms: 			' + Modernizr.csstransforms);
+		trace('css 3d transforms: 			' + Modernizr.csstransforms3d);
+		trace('css transitions: 			' + Modernizr.csstransitions);
+		trace('geolocation: 					' + Modernizr.geolocation);
+		trace(Modernizr.inputtypes);
+		trace(Modernizr.input);
+		trace('local storage: 				' + Modernizr.localstorage);
+		trace('session storage: 			' + Modernizr.sessionstorage);
+		trace('web workers: 					' + Modernizr.webworkers);
+		trace('application cache: 			' + Modernizr.applicationcache);
+		trace('svg: 							' + Modernizr.svg);
+		trace('svg clip paths: 				' + Modernizr.svgclippaths);
+		trace('smil: 							' + Modernizr.smil);
+		trace('web sql database: 			' + Modernizr.websqldatabase);
+		trace('indexed db: 					' + Modernizr.indexeddb);
+		trace('web sockets: 					' + Modernizr.websockets);
+		trace('hashchange event: 			' + Modernizr.hashchange);
+		trace('history management: 		' + Modernizr.historymanagement);
+		trace('drag and drop: 				' + Modernizr.draganddrop);
+		trace('cross window messaging: 	' + Modernizr.crosswindowmessaging);
+		
+	}
+	
+}
---
+++ com/modernizr/Modernizr.hx
@@ -0,0 +1,262 @@
+/**
+ * ...
+ * @author Skial Bainn
+ */
+
+package com.modernizr;
+
+@:native('Modernizr')
+extern class Modernizr {
+
+	static public function __init__():Void untyped {
+		window.Modernizr  = Modernizr;
+	}
+	
+	/**
+	 * CSS classes .fontface/ .no-fontface
+	 */
+	public static var fontface:Bool;
+	
+	/**
+	 * 
+	 * @param	fn		-	The function which will be callback and passed the value
+	 * 						of fontface
+	 */
+	public static function _fontfaceready(fn:Dynamic):Void;
+	
+	/**
+	 * CSS classes .canvas/ .no-canvas
+	 */
+	public static var canvas:Bool;
+	
+	/**
+	 * CSS classes .canvastext/ .no-canvastext
+	 */
+	public static var canvastext:Bool;
+	
+	/**
+	 * CSS classes .audio/ .no-audio
+	 * CSS classes for audio.[format] not applied
+	 */
+	public static var audio = Audio;
+	
+	/**
+	 * CSS classes .video/ .no-video
+	 * CSS classes for video.[format] not applied
+	 */
+	public static var video = Video;
+	
+	/**
+	 * CSS classes .rgba/ .no-.rgba
+	 */
+	public static var rgba:Bool;
+	
+	/**
+	 * CSS classes .hsla/ .no-hsla
+	 */
+	public static var hsla:Bool;
+	
+	/**
+	 * CSS classes .borderimage/ .no-borderimage
+	 */
+	public static var borderimage:Bool;
+	
+	/**
+	 * CSS classes .borderradius/ .no-borderradius
+	 */
+	public static var borderradius:Bool;
+	
+	/**
+	 * CSS classes .boxshadow/ .no-boxshadow
+	 */
+	public static var boxshadow:Bool;
+	
+	/**
+	 * CSS classes .multiplebgs/ .no-multiplebgs
+	 */
+	public static var multiplebgs:Bool;
+	
+	/**
+	 * CSS classes .backgroundsize/ .no-backgroundsize
+	 */
+	public static var backgroundsize:Bool;
+	
+	/**
+	 * CSS classes .opacity/ .no-opacity
+	 */
+	public static var opacity:Bool;
+	
+	/**
+	 * CSS classes .cssanimations/ .no-cssanimations
+	 */
+	public static var cssanimations:Bool;
+	
+	/**
+	 * CSS classes .csscolumns/ .no-csscolumns
+	 */
+	public static var csscolumns:Bool;
+	
+	/**
+	 * CSS classes .cssgradients/ .no-cssgradients
+	 */
+	public static var cssgradients:Bool;
+	
+	/**
+	 * CSS classes .cssreflections/ .no-cssreflections
+	 */
+	public static var cssreflections:Bool;
+	
+	/**
+	 * CSS classes .csstransforms/ .no-csstransforms
+	 */
+	public static var csstransforms:Bool;
+	
+	/**
+	 * CSS classes .csstransforms3d/ .no-csstransforms3d
+	 */
+	public static var csstransforms3d:Bool;
+	
+	/**
+	 * CSS classes .csstransitions/ .no-csstransitions
+	 */
+	public static var csstransitions:Bool;
+	
+	/**
+	 * CSS classes .geolocation/ .no-geolocation
+	 */
+	public static var geolocation:Bool;
+	
+	/**
+	 * CSS classes for input types are not applied
+	 */
+	public static var inputtypes = Inputtypes;
+	
+	/**
+	 * CSS classes for input are not applied
+	 */
+	public static var input = Input;
+	
+	/**
+	 * CSS classes .localstorage/ .no-localstorage
+	 */
+	public static var localstorage:Bool;
+	
+	/**
+	 * CSS classes .sessionstorage/ .no-sessionstorage
+	 */
+	public static var sessionstorage:Bool;
+	
+	/**
+	 * CSS classes .webworkers/ .no-webworkers
+	 */
+	public static var webworkers:Bool;
+	
+	/**
+	 * CSS classes .applicationcache/ .no-applicationcache
+	 */
+	public static var applicationcache:Bool;
+	
+	/**
+	 * CSS classes .svg/ .no-svg
+	 */
+	public static var svg:Bool;
+	
+	/**
+	 * CSS classes .svgclippaths/ .no-svgclippaths
+	 */
+	public static var svgclippaths:Bool;
+	
+	/**
+	 * CSS classes .smil/ .no-smil
+	 */
+	public static var smil:Bool;
+	
+	/**
+	 * CSS classes .websqldatabase/ .no-websqldatabase
+	 */
+	public static var websqldatabase:Bool;
+	
+	/**
+	 * CSS classes .indexeddb/ .no-indexeddb
+	 */
+	public static var indexeddb:Bool;
+	
+	/**
+	 * CSS classes .websockets/ .no-websockets
+	 */
+	public static var websockets:Bool;
+	
+	/**
+	 * CSS classes .hashchange/ .no-hashchange
+	 */
+	public static var hashchange:Bool;
+	
+	/**
+	 * CSS classes .historymanagement/ .no-historymanagement
+	 */
+	public static var historymanagement:Bool;
+	
+	/**
+	 * CSS classes .draganddrop/ .no-draganddrop
+	 */
+	public static var draganddrop:Bool;
+	
+	/**
+	 * CSS classes .crosswindowmessaging/ .no-crosswindowmessaging
+	 */
+	public static var crosswindowmessaging:Bool;
+	
+	/**
+	 * 
+	 * @param	str	-	What you want to test for eg 'text-shadow'
+	 * @param	fn		-	The function containing the test
+	 * @return
+	 */
+	public static function addText(str:String, fn:Dynamic):Bool;
+}
+
+@:native('Modernizr.audio')
+private extern class Audio {
+	public static var ogg:Bool;
+	public static var mp3:Bool;
+	public static var wav:Bool;
+	public static var m4a:Bool;
+}
+
+@:native('Modernizr.video')
+private extern class Video {
+	public static var ogg:Bool;
+	public static var h264:Bool;
+}
+
+@:native('Modernizr.inputtypes')
+private extern class Inputtypes {
+	public static var search:Bool;
+	public static var tel:Bool;
+	public static var url:Bool;
+	public static var email:Bool;
+	public static var datetime:Bool;
+	public static var date:Bool;
+	public static var month:Bool;
+	public static var week:Bool;
+	public static var time:Bool;
+	//public static var datetimelocal:Bool; // I cant get datetime-local, still trying to get around this...
+	public static var number:Bool;
+	public static var range:Bool;
+	public static var color:Bool;
+	
+}
+
+@:native('Modernizr.input')
+private extern class Input {
+	public static var autocomplete:Bool;
+	public static var autofocus:Bool;
+	public static var list:Bool;
+	public static var placeholder:Bool;
+	public static var max:Bool;
+	public static var min:Bool;
+	public static var multiple:Bool;
+	public static var pattern:Bool;
+	public static var required:Bool;
+	public static var step:Bool;
+}
